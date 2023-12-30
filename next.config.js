8272a06a8f28d175de7209cf54ade475cc7996a3
Mian Mohid Naeem <113250876+nexcodes@users.noreply.github.com>
2023-12-30 21:10:12 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     endPoint: "https://dev.tahadialthalatheen.com/api",
     NEXTAUTH_URL: "https://quiz-iota-olive.vercel.app/",
     // NEXTAUTH_URL: "http://localhost:3000/",
-    NEXT_PUBLIC_APP_URL: "http://localhost:3000/",
+    NEXT_PUBLIC_APP_URL: "https://quiz-iota-olive.vercel.app/",
     GOOGLE_CLIENT_ID:
       "894198026676-l0nk0ng3tt65l3aue6l4724on0ngnr85.apps.googleusercontent.com",
     GOOGLE_CLIENT_SECRET: "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",
